22030a4e2d4a0e5d068157f08cea0e76cf5644a8
JayWebDevCom <jaiye.s@gmail.com>
2017-04-14 23:06:16 +0100

    Add ex.19 doubles

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,9 +1,21 @@
-require 'boris-bikes'
+require 'bike'
 
 describe Bike do
 
-  it 'new bikes always work' do
+  it 'it works when new' do
     expect(subject.working).to eq true
+  end
+
+  it 'responds to working' do
+    expect(subject).to respond_to :working
+  end
+
+  it 'responds to #report_broken_bike' do
+    expect(subject).to respond_to :report_broken_bike
+  end
+
+  it 'can be reported as broken' do
+    expect(subject.report_broken_bike).to eq false
   end
 
 end
